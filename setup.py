41cab510e7ddf4a7cd2716cef1ffc84d90abfe79
Brian Tomlinson <darthlukan@gmail.com>
2011-06-19 01:35:25 -0700

    commiting ex47 directories and files

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'ex47 game',
+    'author': 'Brian Tomlinson',
+    'url': 'github.com/darthlukan/ex47',
+    'author_email': 'darthlukan@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],       
+    'scripts': [],
+    'name': 'ex47game'
+}
+
+setup(**config)    
